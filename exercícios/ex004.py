f41d48fca3a2dd76fd7c8325a349b75e9bc5ec47
Ana Karolina França <akfranca@icloud.com>
2026-09-22 13:55:11 -0300

    ex 03 e 04

---
+++ exercícios/ex004.py
@@ -0,0 +1,18 @@
+a = input('Digite algo: ')
+print('O tipo primitivo desse valor é: ', type(a))
+print('Só tem espaço?' , (a.isspace()))
+print('É um número?', (a.isnumeric()))
+print('É alfabeto?', (a.isalpha()))
+print('É alfanumerico?', (a.isalnum()))
+print('Está em maiscula?', (a.isupper()))
+print('Está em minuscula?', (a.islower()))
+print('Está capitalizada?', (a.istitle()))
+
+
+
+
+
+
+
+
+
