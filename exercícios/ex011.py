941a73ec12747ea2f689d8ff9ae8295bc2db3fb9
Ana Karolina França <akfranca@icloud.com>
2026-09-26 13:58:54 -0300

    ex 11 e 12

---
+++ exercícios/ex011.py
@@ -0,0 +1,5 @@
+l = float(input('Qual a largura de sua parede? '))
+a = float(input('Qual a altura de sua parede? '))
+area = l*a
+lt = area/2
+print('Você precisará de {} litros para pintar sua parede.'.format(lt))
---
+++ exercícios/ex012.py
@@ -0,0 +1,4 @@
+p = float(input('Qual o valor do produto? '))
+d = (p*5)/100
+v = p - d
+print('O prduto com desconto custa {} reais!'.format(v))
